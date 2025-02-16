ea55b8b47267417583186003cc07adba98643f38
fortishield <161459699+FortiShield@users.noreply.github.com>
2025-02-17 02:36:11 +0600

    Update __init__.py

--- deepcoder/__init__.py
+++ deepcoder/__init__.py
@@ -1,4 +1,2 @@
-# Adding convenience imports to the package
-
-# from deepcoder.tools import code_vector_repository
-# from deepcoder.core.default import on_disk_repository
+from deepcoder.tools import code_vector_repository
+from deepcoder.core.default import on_disk_repository
